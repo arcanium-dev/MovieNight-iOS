6a84a11a76abe9530481f39886899087fe6b5a86
Muazzam9 <58729460+Muazzam9@users.noreply.github.com>
2023-06-13 23:37:36 +0200

    Ticket-29: Home Screen UI Setup (#5)
    * home screen ui
    
    * fix indentation
    
    * reduce repeated code
    
    * more descriptive func name
    
    * more descriptive var names

--- MovieNight-iOS/Controllers/HomeViewController.swift
+++ MovieNight-iOS/Controllers/HomeViewController.swift
@@ -1,44 +1,156 @@
 import UIKit
-import FirebaseAuth
-import Foundation
+import UIImageColors
+
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var thirdCardImageView: UIImageView!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    private var gradientLayer: CAGradientLayer?
+    private var cardIndex = 0
+    private var initialCardCenter: CGPoint = .zero
     var movieManager = MovieManager()
     var movieIdsList: MovieIds?
+    let movieCardArray = [#imageLiteral(resourceName: "Image 4"), #imageLiteral(resourceName: "Image 2"), #imageLiteral(resourceName: "Image 5"), #imageLiteral(resourceName: "Image 1"), #imageLiteral(resourceName: "Image 3"), #imageLiteral(resourceName: "doctorStrange"), #imageLiteral(resourceName: "Image")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUpComponents()
+        setUpGestureRecognizers()
         movieManager.delegate = self
         movieManager.fetchMovies()
+    }
+    
+    
+    func setUpComponents() {
         
-        // Do any additional setup after loading the view.
+        if let firstImage = movieCardArray.first {
+            firstCardImageView.image = firstImage
+            updateGradientBackground()
+        }
+        
+        createDefaultBackground()
+        
+        // Create movieCardImageView programmatically
+        configureImageView(firstCardImageView)
+        configureImageView(thirdCardImageView)
+        configureImageView(secondCardImageView)
+        
+        updateCardImages()
     }
     
-    @IBAction func logOutTapped(_ sender: UIBarButtonItem) {
-        let alertController = UIAlertController (title: nil,
-                                                 message: "Are you sure you want to sign out?",
-                                                 preferredStyle: .actionSheet)
-        alertController.addAction (
-            UIAlertAction (
-                title: "Sign Out",
-                style: .destructive,
-                handler: { (_) in self.signOut()}
-            )
-        )
-        alertController.addAction (UIAlertAction (title: "Cancel", style: .cancel, handler: nil))
-        present (alertController, animated: true, completion: nil)
+    func configureImageView(_ imageView: UIImageView) {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.center = view.center
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
     }
     
-    func signOut() {
-        do {
-            try Auth.auth().signOut()
-            
-            if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate {
-                sceneDelegate.showAuthScreen()
+    private func createDefaultBackground() {
+        gradientLayer = CAGradientLayer()
+        gradientLayer?.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - (tabBarController?.tabBar.frame.height ?? 0) )
+        
+        if let gradientLayer = gradientLayer {
+            view.layer.insertSublayer(gradientLayer, at: 0)
+        }
+    }
+    
+    private func updateCardImages() {
+        let firstImage = movieCardArray[cardIndex]
+        let secondImage = movieCardArray[(cardIndex + 1) % movieCardArray.count]
+        let thirdImage = movieCardArray[(cardIndex + 2) % movieCardArray.count]
+        
+        firstCardImageView.image = firstImage
+        thirdCardImageView.image = secondImage
+        secondCardImageView.image = thirdImage
+        
+        // Set initial positions for the next cards
+        thirdCardImageView.center = CGPoint(x: firstCardImageView.center.x, y: firstCardImageView.center.y - 10)
+        secondCardImageView.center = CGPoint(x: firstCardImageView.center.x, y: firstCardImageView.center.y - 20)
+    }
+    
+    
+    private func updateGradientBackground() {
+        guard let image = firstCardImageView.image else { return }
+        
+        // Create a higher-quality image representation
+        let size = CGSize(width: view.bounds.width * 2, height: view.bounds.height * 2)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        
+        image.draw(in: CGRect(origin: .zero, size: size))
+        guard let highQualityImage = UIGraphicsGetImageFromCurrentImageContext() else { return }
+        
+        highQualityImage.getColors { [weak self] colors in
+            DispatchQueue.main.async {
+                guard let colors = colors else { return }
+                self?.gradientLayer?.colors = [colors.background.cgColor,
+                                               colors.primary.cgColor,
+                                               colors.background.cgColor]
             }
-        } catch let error {
-            print("Failed to sign out", error)
+        }
+    }
+    
+    
+    func nextCardImage() {
+        cardIndex = (cardIndex + 1) % movieCardArray.count
+        firstCardImageView.image = movieCardArray[cardIndex]
+        updateGradientBackground()
+    }
+    
+    private func setUpGestureRecognizers() {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        firstCardImageView.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    @objc private func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        guard let card = gestureRecognizer.view else { return }
+        
+        switch gestureRecognizer.state {
+        case .began:
+            initialCardCenter = card.center
+        case .changed:
+            let translation = gestureRecognizer.translation(in: view)
+            card.center = CGPoint(x: initialCardCenter.x + translation.x, y: initialCardCenter.y + translation.y)
+            let screenWidth = view.bounds.width
+            let rotationStrength = min(translation.x / (screenWidth / 2), 1)
+            let rotationAngle = CGFloat.pi / 8 * rotationStrength
+            let transform = CGAffineTransform(rotationAngle: rotationAngle)
+            card.transform = transform
+            // Adjust position of the first card only
+            let cardTranslationX = card.center.x - initialCardCenter.x
+            card.center.x = initialCardCenter.x + cardTranslationX
+        case .ended:
+            // Determine if the card was swiped to the left or right
+            let translation = gestureRecognizer.translation(in: view)
+            let velocity = gestureRecognizer.velocity(in: view)
+            let threshold: CGFloat = 100
+            let shouldDismiss = abs(translation.x) > threshold || abs(velocity.x) > threshold
+            
+            if shouldDismiss {
+                // Card was swiped, animate offscreen
+                let destinationX = translation.x > 0 ? view.bounds.width + card.bounds.width : -view.bounds.width - card.bounds.width
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center.x = destinationX
+                }) { _ in
+                    self.nextCardImage()
+                    // Reset card position
+                    card.center = self.initialCardCenter
+                    card.transform = .identity
+                    
+                    self.updateCardImages()
+                }
+            } else {
+                // Card was not swiped, animate back to initial position
+                UIView.animate(withDuration: 0.3) {
+                    card.center = self.initialCardCenter
+                    card.transform = .identity
+                    self.thirdCardImageView.center = self.initialCardCenter
+                    self.secondCardImageView.center = self.initialCardCenter
+                }
+            }
+        default:
+            break
         }
     }
 }
--- MovieNight-iOS/Controllers/ProfileViewController.swift
+++ MovieNight-iOS/Controllers/ProfileViewController.swift
@@ -1,16 +1,29 @@
 import UIKit
-
+import FirebaseAuth
 class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureViewComponents()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    func configureViewComponents() {
+    @IBAction func logOutTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: nil, message: "Are you sure you want to sign out?", preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { [weak self] _ in
+            self?.signOut()
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
-
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            
+            if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate {
+                sceneDelegate.showAuthScreen()
+            }
+        } catch let error {
+            print("Failed to sign out", error)
+        }
+    }
 }
